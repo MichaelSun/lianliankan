069ea9fbdaae328a68920828f4270a46d4372d7f
guoqing.sun <guoqing.sun@renren-inc.com>
2013-03-27 14:42:31 +0800

    删除无用文件

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/michael/github/lianliankan/bin/AndroidManifest.xml : \
-/Users/michael/github/lianliankan/AndroidManifest.xml \
--- bin/lianliankan-release-unsigned.apk.d
+++
@@ -1,9 +0,0 @@
-/Users/michael/github/lianliankan/bin/lianliankan-release-unsigned.apk : \
-/Users/michael/github/lianliankan/bin/lianliankan.ap_ \
-/Users/michael/github/lianliankan/bin/classes.dex \
-/Users/michael/mybackup/software/android-sdk-mac_x86/tools/support/annotations.jar \
-/Users/michael/github/lianliankan/libs/AndroidAnalyticSDK3.1.1.jar \
-/Users/michael/github/lianliankan/libs/youmi-appoffer_v2.1_a_2012-01-31.jar \
-/Users/michael/github/lianliankan/libs/WiGame.jar \
-/Users/michael/github/lianliankan/libs/android-support-v4.jar \
-/Users/michael/github/lianliankan/libs/Renren_SDK_SocialPlugin_Android_v2.1.0.jar \
